17293fd0b2e8535a14a5dce0ba5682e498636e53
Sasha <ani.webdevelopment@gmail.com>
2021-08-16 12:42:20 +0400

    first comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ElementsModule } from './elements/elements.module';
+import { CollectionsModule } from './collections/collections.module';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+   
+    ElementsModule,
+    CollectionsModule,
     AppRoutingModule
   ],
   providers: [],
